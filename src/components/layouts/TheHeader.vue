e440cb4b430c9ea0bfcb28e15b96dbb0ba77b652
adeel <adeel.bandesha148@gmail.com>
2024-07-28 22:03:37 +0500

    change name

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -3,7 +3,7 @@
 		<div class="flex flex-wrap flex-row items-center justify-between md:mx-16">
 			<a class="flex title-font font-medium items-center text-gray-900 md:mb-0 text-3xl">
 				<AppProfile class="w-16 mx-2 px-1" />
-				<h1>Usama<span class="text-blue-700">Saeed</span></h1>
+				<h1>Muhammad<span class="text-blue-700">Usama</span></h1>
 			</a>
 		</div>
 		<Navigation />
